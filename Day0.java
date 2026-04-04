1e0d4ef8c52e9b8f11e4166ce09f5820cff6711d
kavisree-dev <kaviamuthan1405@gmail.com>
2026-04-04 22:45:11 +0530

    Day0 - First java program Hello World

---
+++ Day0.java
@@ -0,0 +1,7 @@
+public class Day0{
+    public static void main(String[] args){
+        System.out.println("hello world");
+        System.out.println("My name is Kavi Sree A");
+        System.out.println("My java journey starts today!");
+    }
+}
