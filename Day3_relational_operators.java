18d66950fdc1e5303add2916a8404bb9ceb3a383
kavisree-dev <kaviamuthan1405@gmail.com>
2026-04-07 22:10:14 +0530

    relational operators

---
+++ Day3_relational_operators.java
@@ -0,0 +1,12 @@
+public class Day3_relational_operators {
+    public static void main(String[] args){
+        int a=10;
+        int b=7;
+        System.out.println(a<b);
+        System.out.println(a>b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+    }
+}
