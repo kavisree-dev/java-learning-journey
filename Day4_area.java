5140aee63cd5b883ecd2f5319f4da5e7cf25a97a
kavisree-dev <kaviamuthan1405@gmail.com>
2026-04-08 22:30:33 +0530

    created area program for user input

---
+++ Day4_area.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Day4_area {
+    public static void main(String[] args){
+    //calculating area of a rectangle
+    double width = 0;
+    double height = 0;
+    double area = 0;
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("enter width:");
+    width=scanner.nextDouble();
+    System.out.print("enter height:");
+    height=scanner.nextDouble();
+    area = width*height;
+    System.out.println("the width is:"+width);
+    System.out.println("the height is:"+height);
+    System.out.println("the area is:"+area+"cm^2");
+    }
+    
+}
