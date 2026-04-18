bec783bd38a38d09403106a498241488f7407762
kavisree-dev <kaviamuthan1405@gmail.com>
2026-04-18 19:44:54 +0530

    if-else condition

---
+++ if_else.java
@@ -0,0 +1,13 @@
+public class if_else{
+    public static void main(String[] args)
+{
+    //if-else condition
+    boolean rain=true;
+    if(rain){
+        System.out.println("Take an umbrella");
+    }
+    else{
+        System.out.println("Enjoy sunlight");
+    }
+}
+}
