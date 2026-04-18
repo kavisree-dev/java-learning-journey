a2b298dba67d39871643c245c20853e1a5b33397
kavisree-dev <kaviamuthan1405@gmail.com>
2026-04-18 23:12:15 +0530

    comparing two strings

---
+++ two_strings_comp.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class two_strings_comp {
+    public static void main(String[] args){
+        //int a=10;//store in stack
+        String name="sree";//store in a string pool of heap eg abc
+        String name1="sree";// in abc
+        String n1= new String("sree");//in ac
+        String n2= new String("sree");//in ab
+        System.out.println(name1.equals(name));//this is correct --abc==abc
+        System.out.println(name1==name);//this is correct for respective string method --abc==abc
+        System.out.println(n1.equals(n2));//this is correct --sree==sree
+        System.out.println(n1==n2); //--ac==ab notequal
+        System.out.println(name1.equals(n1));//this is correct --sree==sree
+        System.out.println(name1==n1);//abc==ac not equals
+    }
+}
