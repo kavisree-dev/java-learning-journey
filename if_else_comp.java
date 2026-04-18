39ea38085375f131c8a8e99c403bf3ae67b30f89
kavisree-dev <kaviamuthan1405@gmail.com>
2026-04-18 21:25:41 +0530

    if else condition with integer family using comparision operators

---
+++ if_else_comp.java
@@ -0,0 +1,61 @@
+public class if_else_comp {
+    public static void main(String[] args){
+        //comparision operators using if else condition for integers family
+        
+        float float1 = 1.3f;
+        float float2= 1.5f;
+        double double1 = 23.000;
+        double double2 = 23.456;
+        long long1 = 200000000000000L;
+        long long2 = 100000000000000L;
+        int int1 = 355668787;
+        int int2 = 1556687875;
+        short short1 = 30000;
+        short short2 = 20000;
+        byte byte1 = 122;
+        byte byte2 = -120;
+ if(float1>float2){
+             System.out.println("the float1 is greater");
+ }
+        else{
+             System.out.println("the float2 is greater");
+ 
+ }
+ if(double1<double2){
+             System.out.println("the double1 is small");
+ }
+        else{
+             System.out.println("the double2 is small");
+ }
+  if(long1==long2){
+             System.out.println("the long1 and long2 are equal");
+ }
+        else{
+             System.out.println("not equal");
+ }
+  if(int1>=int2){
+             System.out.println("the int1 is greater than or eqyal to int2 ");
+ }
+        else{
+             System.out.println("the int1 is not greater than or equal to int2 ");
+ }
+  if(short1<=short2){
+             System.out.println("the short1 is less than or equal to short2");
+ }
+        else{
+             System.out.println("the short1 is not less than or equal to short2 ");
+ }
+  if(byte1!=byte2){
+             System.out.println("the byte1 is not equal to byte2");
+ }
+        else{
+             System.out.println("the byte1 is equal to byte2");
+ }
+  if(int1>float2){
+             System.out.println("the int1 is greater");
+ }
+        else{
+             System.out.println("the float2 is greater");
+ }
+    }
+}
