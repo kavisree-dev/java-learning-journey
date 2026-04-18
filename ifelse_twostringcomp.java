d17999ead7b580008fe211ff4f638d151f3ee8b0
kavisree-dev <kaviamuthan1405@gmail.com>
2026-04-18 23:11:03 +0530

    using if else condition comparing two strings

---
+++ ifelse_twostringcomp.java
@@ -0,0 +1,11 @@
+public class ifelse_twostringcomp{
+    public static void main(String[] args){
+        String a1="condition";
+        String a2=new String("condition");
+        if(a1.equals(a2)){
+        System.out.println("a1 and a2 are equal");}
+        else{
+             System.out.println("a1 and a2 are not equal");}
+
+        }
+    }
