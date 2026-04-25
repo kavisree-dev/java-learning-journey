eadd2e3f4551fcee4d63a8e899b0f8feda9af0b2
kavisree-dev <kaviamuthan1405@gmail.com>
2026-04-25 10:27:40 +0530

    if else only with fail/pass mark evaluation program

--- ifelsepractice.java
+++ ifelsepractice.java
@@ -9,18 +9,18 @@ int social=evaluation.nextInt();
 System.out.print("Enter kavi's science mark:");
 int science=evaluation.nextInt();
 if(maths>=35)
-    System.out.println("kavi's maths mark is "+maths +". She passed the maths exam");
+    System.out.println("kavi's maths mark is "+maths +". She passed the maths exam.");
 else
     System.out.println("she got fail mark in the maths exam.");
 if(social>=35)
-    System.out.println("kavi's social mark is "+social +". She passed the social exam");
+    System.out.println("kavi's social mark is "+social +". She passed the social exam.");
 else
     System.out.println("she got fail mark in the social exam.");
 if(science>=35)
-    System.out.println("kavi's science mark is "+science +". She passed the science exam");
+    System.out.println("kavi's science mark is "+science +". She passed the science exam.");
 else
     System.out.println("she got fail mark in the science exam.");
-
+evaluation.close();
     }
     
 }
