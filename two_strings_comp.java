0f5165c759953b71758809491b347c789d544726
kavisree-dev <kaviamuthan1405@gmail.com>
2026-04-26 13:28:51 +0530

    updated files

--- two_strings_comp.java
+++ two_strings_comp.java
@@ -6,7 +6,7 @@ public class two_strings_comp {
         String name1="sree";// in abc
         String n1= new String("sree");//in ac
         String n2= new String("sree");//in ab
-        System.out.println(name1.equals(name));//this is correct --abc==abc
+        System.out.println(name1.equals(name));//this is correct --sree==sree
         System.out.println(name1==name);//this is correct for respective string method --abc==abc
         System.out.println(n1.equals(n2));//this is correct --sree==sree
         System.out.println(n1==n2); //--ac==ab notequal
